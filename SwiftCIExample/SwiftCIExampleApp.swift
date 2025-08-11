c181ac1dbde3112da3d34146d72e4f724e404127
vickeyrajesh <vickey.rajesh@gmail.com>
2025-08-11 13:34:14 +0530

    Initial Commit

---
+++ SwiftCIExample/SwiftCIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCIExampleApp.swift
+//  SwiftCIExample
+//
+//  Created by Rajesh Yadav on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
